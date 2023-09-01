241f7a8865e38dfaa9742e665a59c446c5ab303f
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-09-01 18:07:20 +0500

    added feature to go to home screen after login in or signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:memories_app/models/theme.dart';
 import 'package:memories_app/screens/auth_screen.dart';
+import 'package:memories_app/screens/home_screen.dart';
+import 'package:memories_app/screens/splash_screen.dart';
 
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
@@ -20,7 +23,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: lightTheme,
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
