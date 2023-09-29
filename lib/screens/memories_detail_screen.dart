e7fd8922f552176aba6bcc0cf157c227b08f86b5
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-09-29 21:41:54 +0500

    completed detail screen with location showing

--- lib/screens/memories_detail_screen.dart
+++ lib/screens/memories_detail_screen.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class MemoryDetailsScreen extends StatelessWidget {
   const MemoryDetailsScreen({
     required this.memoryId,
+    super.key,
   });
 
   final String memoryId;
@@ -23,17 +24,17 @@ class MemoryDetailsScreen extends StatelessWidget {
         future: fetchMemoryDetails(),
         builder: (ctx, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           }
 
           if (!snapshot.hasData || !snapshot.data!.exists) {
-            return Center(child: Text('Memory not found.'));
+            return const Center(child: Text('Memory not found.'));
           }
 
           final memoryData = snapshot.data!.data() as Map<String, dynamic>;
 
-          final lat = memoryData['location']['latitude'] as double;
-          final lng = memoryData['location']['longitude'] as double;
+          final lat = memoryData['latitude'] as double;
+          final lng = memoryData['longitude'] as double;
 
           final locationImage =
               'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyB-qF_ODijQOhNfpfI2IxmeIjYw0LeY5OE';
@@ -50,41 +51,44 @@ class MemoryDetailsScreen extends StatelessWidget {
                 bottom: 0,
                 left: 0,
                 right: 0,
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        // Handle map tap if needed
-                      },
-                      child: CircleAvatar(
-                        radius: 70,
-                        backgroundImage: NetworkImage(locationImage),
-                      ),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black45,
+                        Colors.black87,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
                     ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 16,
-                        horizontal: 24,
-                      ),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.black45,
-                            Colors.black87,
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
+                  ),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle map tap if needed
+                        },
+                        child: CircleAvatar(
+                          radius: 70,
+                          backgroundImage: NetworkImage(locationImage),
                         ),
                       ),
-                      child: Text(
-                        memoryData['address'],
-                        textAlign: TextAlign.center,
-                        style: style.textTheme.titleMedium!.copyWith(
-                          color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 16,
+                          horizontal: 24,
+                        ),
+                        child: Text(
+                          memoryData['address'],
+                          textAlign: TextAlign.center,
+                          style: style.textTheme.titleMedium!.copyWith(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -96,11 +100,11 @@ class MemoryDetailsScreen extends StatelessWidget {
           future: fetchMemoryDetails(),
           builder: (ctx, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text('Loading...');
+              return const Text('Loading...');
             }
 
             if (!snapshot.hasData || !snapshot.data!.exists) {
-              return Text('Memory not found.');
+              return const Text('Memory not found.');
             }
 
             final memoryData = snapshot.data!.data() as Map<String, dynamic>;
