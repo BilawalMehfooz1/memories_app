03d65773b97204cee4379041d7f1fd5e3e9f1688
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-09-30 22:26:19 +0500

    setting up theming

--- lib/screens/memories_detail_screen.dart
+++ lib/screens/memories_detail_screen.dart
@@ -39,63 +39,67 @@ class MemoryDetailsScreen extends StatelessWidget {
           final locationImage =
               'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyB-qF_ODijQOhNfpfI2IxmeIjYw0LeY5OE';
 
-          return Stack(
-            children: [
-              Image.network(
-                memoryData['imageUrl'],
-                fit: BoxFit.cover,
-                height: double.infinity,
-                width: double.infinity,
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.transparent,
-                        Colors.black45,
-                        Colors.black87,
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          // Handle map tap if needed
-                        },
-                        child: CircleAvatar(
-                          radius: 70,
-                          backgroundImage: NetworkImage(locationImage),
-                        ),
+          return Container(
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Stack(
+              children: [
+                Image.network(
+                  memoryData['imageUrl'],
+                  fit: BoxFit.contain,
+                  height: double.infinity,
+                  width: double.infinity,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black45,
+                          Colors.black87,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 16,
-                          horizontal: 24,
-                        ),
-                        child: Text(
-                          memoryData['address'],
-                          textAlign: TextAlign.center,
-                          style: style.textTheme.titleMedium!.copyWith(
-                            color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // Handle map tap if needed
+                          },
+                          child: CircleAvatar(
+                            radius: 70,
+                            backgroundImage: NetworkImage(locationImage),
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 24,
+                          ),
+                          child: Text(
+                            memoryData['address'],
+                            textAlign: TextAlign.center,
+                            style: style.textTheme.titleMedium!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         },
       ),
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: FutureBuilder<DocumentSnapshot>(
           future: fetchMemoryDetails(),
           builder: (ctx, snapshot) {
@@ -108,7 +112,10 @@ class MemoryDetailsScreen extends StatelessWidget {
             }
 
             final memoryData = snapshot.data!.data() as Map<String, dynamic>;
-            return Text(memoryData['title']);
+            return Text(
+              memoryData['title'],
+              style: const TextStyle(color: Colors.white),
+            );
           },
         ),
       ),
