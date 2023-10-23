e9416183451dbc575112ea26c4835cd4ab132814
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-10-23 20:34:43 +0500

    added design in filters

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:memories_app/providers/selection_notifier.dart';
@@ -17,6 +18,29 @@ class HomeScreen extends ConsumerStatefulWidget {
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   MemoryFilter _selectedFilter = MemoryFilter.all;
   DateTime? _selectedMonthYear = DateTime.now();
+  final ScrollController _scrollController = ScrollController();
+  bool _showFilter = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+              ScrollDirection.reverse &&
+          _showFilter) {
+        setState(() {
+          _showFilter = false;
+        });
+      } else if (_scrollController.position.userScrollDirection ==
+              ScrollDirection.forward &&
+          !_showFilter) {
+        setState(() {
+          _showFilter = true;
+        });
+      }
+    });
+  }
 
   Widget _buildShimmerItem() {
     return Shimmer.fromColors(
@@ -63,65 +87,56 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
               .where('createdAt', isGreaterThanOrEqualTo: start)
               .where('createdAt', isLessThan: end)
               .snapshots();
+        default:
+          return query.snapshots();
       }
     }
 
-    return Column(
-      children: [
-        Container(
-          color: Colors.black,
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _filterButton('All', MemoryFilter.all),
-              _filterButton('Newest', MemoryFilter.newest),
-              _filterButton('Oldest', MemoryFilter.oldest),
-              _filterButton('Month & Year', MemoryFilter.monthYear,
-                  additionalLogic: () {
-                showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime.now(),
-                ).then((pickedDate) {
-                  if (pickedDate != null) {
-                    setState(() {
-                      _selectedMonthYear = pickedDate;
-                      _selectedFilter = MemoryFilter.monthYear;
-                    });
-                  }
-                });
-              }),
-            ],
-          ),
-        ),
-        Expanded(
-          child: StreamBuilder<QuerySnapshot>(
-            stream: getMemoriesStream(),
-            builder: (ctx, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return GridView.builder(
-                  padding: const EdgeInsets.all(10),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
+    return Scaffold(
+      body: Column(
+        children: [
+          // Divider(color: Colors.grey[300]),
+          if (_showFilter)
+            Column(
+              children: [
+                Container(
+                  color: Colors.transparent,
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _filterButton('All', MemoryFilter.all),
+                      _filterButton('Newest', MemoryFilter.newest),
+                      _filterButton('Oldest', MemoryFilter.oldest),
+                      _filterButton('Month & Year', MemoryFilter.monthYear,
+                          additionalLogic: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime.now(),
+                        ).then((pickedDate) {
+                          if (pickedDate != null) {
+                            setState(() {
+                              _selectedMonthYear = pickedDate;
+                              _selectedFilter = MemoryFilter.monthYear;
+                            });
+                          }
+                        });
+                      }),
+                    ],
                   ),
-                  itemCount: 6,
-                  itemBuilder: (context, index) => _buildShimmerItem(),
-                );
-              }
-
-              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return const Center(child: Text('No memories found.'));
-              }
-
-              final memories = snapshot.data!.docs;
-
-              return Stack(
-                children: [
-                  GridView.builder(
+                ),
+                Divider(color: Colors.grey[300]),
+              ],
+            ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: getMemoriesStream(),
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return GridView.builder(
+                    controller: _scrollController,
                     padding: const EdgeInsets.all(10),
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
@@ -129,111 +144,135 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       crossAxisSpacing: 10,
                       mainAxisSpacing: 10,
                     ),
-                    itemCount: memories.length,
-                    itemBuilder: (context, index) {
-                      final memory = memories[index];
-                      final isSelected = selectionNotifier.selectedMemories
-                          .contains(memory.id);
+                    itemCount: 6,
+                    itemBuilder: (context, index) => _buildShimmerItem(),
+                  );
+                }
 
-                      return InkWell(
-                        onTap: () {
-                          if (selectionNotifier.isSelecting) {
-                            selectionNotifier.toggleMemorySelection(memory.id);
-                          } else {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    MemoryDetailsScreen(memoryId: memory.id),
-                              ),
-                            );
-                          }
-                        },
-                        onLongPress: () {
-                          selectionNotifier.toggleMemorySelection(memory.id);
-                        },
-                        child: Card(
-                          elevation: 5,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Stack(
-                              children: [
-                                Image.network(
-                                  memory['imageUrl'],
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                  height: double.infinity,
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(child: Text('No memories found.'));
+                }
+
+                final memories = snapshot.data!.docs;
+
+                return Stack(
+                  children: [
+                    GridView.builder(
+                      controller: _scrollController,
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                      ),
+                      itemCount: memories.length,
+                      itemBuilder: (context, index) {
+                        final memory = memories[index];
+                        final isSelected = selectionNotifier.selectedMemories
+                            .contains(memory.id);
+
+                        return InkWell(
+                          onTap: () {
+                            if (selectionNotifier.isSelecting) {
+                              selectionNotifier
+                                  .toggleMemorySelection(memory.id);
+                            } else {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      MemoryDetailsScreen(memoryId: memory.id),
                                 ),
-                                if (isSelected)
-                                  Positioned(
-                                    top: 10,
-                                    right: 10,
-                                    child: Icon(
-                                      Icons.check_circle,
-                                      color: Colors.green[700],
-                                      size: 30.0,
-                                    ),
+                              );
+                            }
+                          },
+                          onLongPress: () {
+                            selectionNotifier.toggleMemorySelection(memory.id);
+                          },
+                          child: Card(
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Stack(
+                                children: [
+                                  Image.network(
+                                    memory['imageUrl'],
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                    height: double.infinity,
                                   ),
-                                Positioned(
-                                  bottom: 0,
-                                  left: 0,
-                                  right: 0,
-                                  child: Container(
-                                    color: Colors.black54,
-                                    padding:
-                                        const EdgeInsets.symmetric(vertical: 5),
-                                    child: Text(
-                                      memory['title'],
-                                      textAlign: TextAlign.center,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
+                                  if (isSelected)
+                                    Positioned(
+                                      top: 10,
+                                      right: 10,
+                                      child: Icon(
+                                        Icons.check_circle,
+                                        color: Colors.green[700],
+                                        size: 30.0,
+                                      ),
+                                    ),
+                                  Positioned(
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    child: Container(
+                                      color: Colors.black54,
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 5),
+                                      child: Text(
+                                        memory['title'],
+                                        textAlign: TextAlign.center,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                  if (selectionNotifier.isSelecting)
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Center(
-                        child: Container(
-                          width: double.infinity,
-                          height: 60,
-                          color: Colors.white,
-                          child: Center(
-                            child: Container(
-                              width: 100,
-                              color: Colors.transparent,
-                              child: IconButton(
-                                icon: const Icon(
-                                  Icons.delete,
-                                  color: Colors.black,
-                                  size: 35,
+                        );
+                      },
+                    ),
+                    if (selectionNotifier.isSelecting)
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: Container(
+                            width: double.infinity,
+                            height: 60,
+                            color: Colors.white,
+                            child: Center(
+                              child: Container(
+                                width: 100,
+                                color: Colors.transparent,
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.delete,
+                                    color: Colors.black,
+                                    size: 35,
+                                  ),
+                                  onPressed: deleteSelectedMemories,
                                 ),
-                                onPressed: deleteSelectedMemories,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    )
-                ],
-              );
-            },
+                      )
+                  ],
+                );
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -241,8 +280,10 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       {VoidCallback? additionalLogic}) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        primary: _selectedFilter == filter ? Colors.white : Colors.grey[300],
-        onPrimary: _selectedFilter == filter ? Colors.black : Colors.black54,
+        backgroundColor:
+            _selectedFilter == filter ? Colors.white : Colors.transparent,
+        foregroundColor:
+            _selectedFilter == filter ? Colors.black : Colors.black54,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
