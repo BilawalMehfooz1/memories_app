5660432f5612a65cb0ce5c51a0c09de77dec62a7
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-10-25 12:04:21 +0500

    adding setting and profile method

--- lib/providers/progress_indicator_provider.dart
+++
@@ -1,4 +0,0 @@
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final uploadProgressProvider = StateProvider<double>((ref) => 0.0);
--- lib/providers/tabscreen_provider.dart
+++ lib/providers/tabscreen_provider.dart
@@ -1,11 +1,12 @@
 import 'package:tuple/tuple.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:memories_app/screens/home_screen.dart';
 import 'package:memories_app/screens/favorites_screen.dart';
 import 'package:memories_app/screens/add_memory_screen.dart';
+
+enum MenuOptions { profile, setting }
 
 class TabScreenNotifier extends StateNotifier<int> {
   TabScreenNotifier() : super(0);
@@ -24,10 +25,46 @@ class TabScreenNotifier extends StateNotifier<int> {
         List<Widget> homeActions = [
           IconButton(
             onPressed: () {
-              FirebaseAuth.instance.signOut();
+              // FirebaseAuth.instance.signOut();
             },
-            icon: const Icon(Icons.exit_to_app),
+            icon: const Icon(Icons.menu_book_rounded),
           ),
+          PopupMenuButton<MenuOptions>(
+            offset: const Offset(0, 50),
+            elevation: 3.2,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            icon: const Icon(Icons.more_vert),
+            onSelected: (MenuOptions result) {
+              switch (result) {
+                case MenuOptions.profile:
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => Placeholder(),
+                    ),
+                  );
+                  break;
+                case MenuOptions.setting:
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => Placeholder(),
+                    ),
+                  );
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<MenuOptions>>[
+              const PopupMenuItem<MenuOptions>(
+                value: MenuOptions.profile,
+                child: Text('Profile'),
+              ),
+              const PopupMenuItem<MenuOptions>(
+                value: MenuOptions.setting,
+                child: Text('Settings'),
+              ),
+            ],
+          )
         ];
         return Tuple3(const HomeScreen(), 'Memories', homeActions);
     }
